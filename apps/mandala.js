da5614219a444acfaed255bdb734ad3487b64e3e
callumfowler101 <callumfowler101@gmail.com>
2020-08-08 16:16:58 +0100

    added mandala --interaction

---
+++ apps/mandala.js
@@ -0,0 +1,65 @@
+let sketch3 = (m) => {
+    m.web_style = document.getElementById('sketch3');
+    m.clearBut = document.getElementById('clearBut');
+    m.saveBut = document.getElementById('saveBut');
+    m.docSz;
+    m.segments;
+
+    m.setup = () => {
+        m.docSz = m.web_style.clientWidth-5;
+        let canvas = m.createCanvas(m.docSz, m.docSz);
+        canvas.position(m.web_style.offsetLeft+2.5, m.web_style.offsetTop);
+
+
+        m.segments = m.floor(m.random(4,20));
+        m.background(0);
+    }
+
+    m.windowResized = () => {
+        m.setup();
+    }
+
+    m.clearBut.addEventListener("click", ()=>{
+        m.background(0);
+        m.segments = m.floor(m.random(4,20));
+    });
+
+    m.saveBut.addEventListener("click", ()=>{
+        m.save("mandala.jpg");
+    });
+
+    m.draw = () => {
+        m.translate(m.width/2,m.height/2);
+        let d = m.dist(m.mouseX,m.mouseY,m.pmouseX,m.pmouseY);
+        let sW = m.map(d, 0, 20, 5, 0);
+        let rotationDiv = 360/m.segments;
+        m.strokeWeight(sW);
+        m.stroke(255);
+        
+        if(m.mouseIsPressed){
+            if(m.mouseX>0&&m.mouseX<m.width&&m.mouseY>0&&m.mouseY<m.height){
+                for(let i=0; i<m.segments; i++){
+                    m.push();
+                    m.rotate(i*rotationDiv);
+                    m.line(m.mouseX-m.width/2, m.mouseY-m.height/2, m.pmouseX-m.width/2, m.pmouseY-m.height/2);
+                    m.pop();
+                }
+            }
+        }
+    }
+
+
+    m.disableScroll = () => {
+        document.body.addEventListener('touchmove', o.preventDefault, { passive: false });
+    }
+
+    m.enableScroll = () => {
+        document.body.removeEventListener('touchmove', o.preventDefault, { passive: false });
+    }
+
+    m.preventDefault = (e) => {
+        e.preventDefault();
+    }
+}
+
+let mandala = new p5(sketch3);
